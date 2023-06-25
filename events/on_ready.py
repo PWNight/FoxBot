3538f34dee0e9dc629900c36a93690e6a16c60fd
PWNight <rodionvarzymov@outlook.com>
2023-06-25 11:20:16 +0300

    Удалена выдача роли "Шизоид"

--- events/on_ready.py
+++ events/on_ready.py
@@ -47,8 +47,7 @@ class OnReady(commands.Cog):
         emb2 = discord.Embed(title='🔔 Уведомления и особые роли', description= '''> 📰 — оповещения о новостях проекта в канале <#939438314954588201>.
         > 📆 — оповещения о предстоящих событиях проекта в канале  <#1100414892609130527>.
         
-        > 🔓 — доступ к категории с каналами #скриншоты и #игра предыдущих сезонов.
-        > 🗿 — доступ к каналу <#1064971796203446423>, место содержания чудиков проекта''', colour = 0xecac4b)
+        > 🔓 — доступ к категории с каналами #скриншоты и #игра предыдущих сезонов.''', colour = 0xecac4b)
         row = Button(
                 style = discord.ButtonStyle.gray,
                 custom_id = 'news',
@@ -62,18 +61,12 @@ class OnReady(commands.Cog):
         row3 = Button(
                 style = discord.ButtonStyle.gray,
                 custom_id = 'access',
-                emoji= '🔓'
-            )
-        row4 = Button(
-                style = discord.ButtonStyle.gray,
-                custom_id = 'durka',
-                emoji= '🗿'
+                emoji= '📦'
             )
         view=View()
         view.add_item(row)
         view.add_item(row2)
         view.add_item(row3)
-        view.add_item(row4)
         await notifymsg.edit(embed = emb2, view=view)
         self.status_task.start() 
 
@@ -91,8 +84,7 @@ class OnReady(commands.Cog):
         emb2 = discord.Embed(title='🔔 Уведомления и особые роли', description= '''> 📰 — оповещения о новостях проекта в канале <#939438314954588201>.
         > 📆 — оповещения о предстоящих событиях проекта в канале  <#1100414892609130527>.
         
-        > 🔓 — доступ к категории с каналами #скриншоты и #игра предыдущих сезонов.
-        > 🗿 — доступ к каналу <#1064971796203446423>, место содержания чудиков проекта''', colour = 0xecac4b)
+        > 📦 — доступ к категории с каналами #скриншоты и #игра предыдущих сезонов.''', colour = 0xecac4b)
         await ctx.send(embed=emb1)
         await ctx.send(embed=emb2)
 
@@ -194,19 +186,6 @@ class OnReady(commands.Cog):
             role = discord.utils.get(guild.roles, id=1035615119213854803)
             if role in memberop.roles:
                 role = discord.utils.get(guild.roles, id=1035615119213854803)
-                await memberop.remove_roles(role)
-                await inter.send(resno, ephemeral = True)
-                return
-            if not role in memberop.roles:
-                await memberop.add_roles(role)
-                await inter.send(resyes, ephemeral = True)
-
-        if inter.component.custom_id == "durka":
-            resyes = '<a:phoenix_toggleon:953725340042293369> Роль <@&1077928906235064320> успешно выдана.'
-            resno = '<a:phoenix_toggleoff:953725338347782145> Роль <@&1077928906235064320> успешно снята.'
-            role = discord.utils.get(guild.roles, id=1077928906235064320)
-            if role in memberop.roles:
-                role = discord.utils.get(guild.roles, id=1077928906235064320)
                 await memberop.remove_roles(role)
                 await inter.send(resno, ephemeral = True)
                 return
