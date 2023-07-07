0ee2318e6188ad5667ba5f1ac41d49e30e8e41c0
PWNight <rodionvarzymov@outlook.com>
2023-07-07 13:50:35 +0300

    Миграция выдачи ролей в отдельный файл

---
+++ events/roles_on_button_click.py
@@ -0,0 +1,130 @@
+import disnake as discord
+from disnake.ext import commands
+import asyncio
+import os
+from disnake import File
+from disnake.ui import Button, View
+
+class RolesButtonClick(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def notifysetup(self,ctx):
+        emb1 = discord.Embed(title="", colour = 0xecac4b)
+        emb1.set_image(url='https://media.discordapp.net/attachments/1053188377651970098/1107317077364178944/d7b96329cadf7c8b.png')
+        emb2 = discord.Embed(title='🔔 Уведомления и особые роли', description= '''> 📰 — оповещения о новостях проекта в канале <#939438314954588201>.
+        > 📆 — оповещения о предстоящих событиях проекта в канале  <#1100414892609130527>.
+        
+        > 📦 — доступ к категории с каналами #скриншоты и #игра предыдущих сезонов.''', colour = 0xecac4b)
+        await ctx.send(embed=emb1)
+        await ctx.send(embed=emb2)
+
+
+    @commands.Cog.listener()
+    async def on_button_click(self, inter):
+        #Техническая информация для выдачи ролей.
+        guild = self.client.get_guild(inter.guild.id)
+        memberop = inter.author
+
+        if inter.component.custom_id == "news":
+                chooseemb = discord.Embed(title='📰 Выберите сервер, по которому желаете получать новости.', color = 0x607aff)
+                row = Button(
+                        style = discord.ButtonStyle.blurple,
+                        label = 'Discord',
+                        custom_id = 'discord_news',
+                        emoji= '<:discord:856561477033263124>'
+                    )
+                row2 = Button(
+                        style = discord.ButtonStyle.green,
+                        label = 'Minecraft',
+                        custom_id = 'minecraft_news',
+                        emoji= '<:minecraft:856561476873355316>'
+                    )
+                view=View()
+                view.add_item(row)
+                view.add_item(row2)
+                await inter.send(embed=chooseemb, view = view, ephemeral = True)
+                return
+        if inter.component.custom_id == "discord_news":
+            resyes = '<a:phoenix_toggleon:953725340042293369> Роль <@&1095191584590549052> успешно выдана.'
+            resno = '<a:phoenix_toggleoff:953725338347782145> Роль <@&1095191584590549052> успешно снята.'
+            discordnewsrole = discord.utils.get(guild.roles, id=1095191584590549052)
+            if discordnewsrole in memberop.roles:
+                await memberop.remove_roles(discordnewsrole)
+                await inter.send(resno, ephemeral = True)
+                return
+            if not discordnewsrole in memberop.roles:
+                await memberop.add_roles(discordnewsrole)
+                await inter.send(resyes, ephemeral = True)
+        if inter.component.custom_id == "minecraft_news":
+            resyes = '<a:phoenix_toggleon:953725340042293369> Роль <@&1095191555972804739> успешно выдана.'
+            resno = '<a:phoenix_toggleoff:953725338347782145> Роль <@&1095191555972804739> успешно снята.'
+            minecraftnewsrole = discord.utils.get(guild.roles, id=1095191555972804739)
+            if minecraftnewsrole in memberop.roles:
+                await memberop.remove_roles(minecraftnewsrole)
+                await inter.send(resno, ephemeral = True)
+                return
+            if not minecraftnewsrole in memberop.roles:
+                await memberop.add_roles(minecraftnewsrole)
+                await inter.send(resyes, ephemeral = True)
+
+        if inter.component.custom_id == "events":
+                chooseemb = discord.Embed(title='📆 Выберите сервер, по которому желаете получать анонсы.', color = 0x607aff)
+                row = Button(
+                        style = discord.ButtonStyle.blurple,
+                        label = 'Discord',
+                        custom_id = 'discord_annonces',
+                        emoji= '<:discord:856561477033263124>'
+                    )
+                row2 = Button(
+                        style = discord.ButtonStyle.green,
+                        label = 'Minecraft',
+                        custom_id = 'minecraft_annonces',
+                        emoji= '<:minecraft:856561476873355316>'
+                    )
+                view=View()
+                view.add_item(row)
+                view.add_item(row2)
+                await inter.send(embed=chooseemb, view = view, ephemeral = True)
+                return
+        if inter.component.custom_id == "discord_annonces":
+            resyes = '<a:phoenix_toggleon:953725340042293369> Роль <@&1095191824898990151> успешно выдана.'
+            resno = '<a:phoenix_toggleoff:953725338347782145> Роль <@&1095191824898990151> успешно снята.'
+            discordannoncesrole = discord.utils.get(guild.roles, id=1095191824898990151)
+            if discordannoncesrole in memberop.roles:
+                await memberop.remove_roles(discordannoncesrole)
+                await inter.send(resno, ephemeral = True)
+                return
+            if not discordannoncesrole in memberop.roles:
+                await memberop.add_roles(discordannoncesrole)
+                await inter.send(resyes, ephemeral = True)
+
+        if inter.component.custom_id == "minecraft_annonces":
+            resyes = '<a:phoenix_toggleon:953725340042293369> Роль <@&951475369041616926> успешно выдана.'
+            resno = '<a:phoenix_toggleoff:953725338347782145> Роль <@&951475369041616926> успешно снята.'
+            minecraftanoncesrole = discord.utils.get(guild.roles, id=951475369041616926)
+            if minecraftanoncesrole in memberop.roles:
+                await memberop.remove_roles(minecraftanoncesrole)
+                await inter.send(resno, ephemeral = True)
+                return
+            if not minecraftanoncesrole in memberop.roles:
+                await memberop.add_roles(minecraftanoncesrole)
+                await inter.send(resyes, ephemeral = True)
+
+        if inter.component.custom_id == "access":
+            resyes = '<a:phoenix_toggleon:953725340042293369> Роль <@&1035615119213854803> успешно выдана.'
+            resno = '<a:phoenix_toggleoff:953725338347782145> Роль <@&1035615119213854803> успешно снята.'
+            role = discord.utils.get(guild.roles, id=1035615119213854803)
+            if role in memberop.roles:
+                role = discord.utils.get(guild.roles, id=1035615119213854803)
+                await memberop.remove_roles(role)
+                await inter.send(resno, ephemeral = True)
+                return
+            if not role in memberop.roles:
+                await memberop.add_roles(role)
+                await inter.send(resyes, ephemeral = True)
+
+def setup(client):
+    client.add_cog(RolesButtonClick(client))
