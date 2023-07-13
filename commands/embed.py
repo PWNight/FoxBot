7492d39cb34b46026c6c330a21471681e1dda7f7
PWNight <rodionvarzymov@outlook.com>
2023-07-13 22:42:52 +0300

    Исправление никнейма автора в команде \\say

--- commands/embed.py
+++ commands/embed.py
@@ -125,7 +125,7 @@ class Embed(commands.Cog):
                             author = author[2:]
                             author = author[:-1]
                         fm2 = await self.client.fetch_user(int(author))
-                        em.set_author(name=f"{ctx.author.display_name}",icon_url=f"{fm2.avatar.url}")
+                        em.set_author(name=f"{fm2.display_name}",icon_url=f"{fm2.avatar.url}")
                 else:
                     pass
             if not author:
