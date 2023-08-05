43d43e6e78d3eda5a75cb30b094541d6e996f0ff
PWNight <rodionvarzymov@outlook.com>
2023-08-05 10:52:15 +0300

    Обновление дизайна заявок верификации

--- events/verify_on_button_click.py
+++ events/verify_on_button_click.py
@@ -68,7 +68,7 @@ class VerifyButtonClick(commands.Cog):
                     return 
                 else:
                     await inter.response.send_message(content='<:minecraft_accept:1080779491875491882> Ваша заявка была отправлена. Решение по вашей заявке будет отправлено вам в ЛС.', ephemeral = True)
-                    embinfo = discord.Embed(title=f'<:invite:1105878276242673725> Информация о заявке игрока {memberop.name}', color = 0x2f3136)
+                    embinfo = discord.Embed(description=f"<:invite:1105878276242673725> Заявка пользователя {memberop.mention} ({memberop.display_name})", color = 0x2f3136)
                     for key, value in inter.text_values.items():
                         embinfo.add_field(name=key.capitalize(), value=value[:1024], inline=False)
                     row = Button(
@@ -105,7 +105,7 @@ class VerifyButtonClick(commands.Cog):
                                 rcon.command(f'easywl add {inter.text_values["Никнейм"]}')
                                 rcon.command(f'Добавление игрока в вайтлист')
                                 rcon.stop()
-                            embinfo = discord.Embed(description=f"<:invite:1105878276242673725> Заявка игрока {memberop.mention} \n\n**Статус заявки:** <:minecraft_accept:1080779491875491882> Принята. \n**Принял заявку:** {m.author.mention}.", color = 0x2f3136)
+                            embinfo = discord.Embed(description=f"<:invite:1105878276242673725> Заявка пользователя {memberop.mention} ({memberop.display_name}) \n\n**Статус заявки:** <:minecraft_accept:1080779491875491882> Принята \n**Принял заявку:** {m.author.mention}", color = 0x2f3136)
                             for key, value in inter.text_values.items():
                                 embinfo.add_field(name=key.capitalize(), value=value[:1024], inline=False)  
                             await logchannel.send(embed=embinfo)
@@ -116,7 +116,7 @@ class VerifyButtonClick(commands.Cog):
                             return
                         if m.component.custom_id == "deny_verify":
                             await msgtic.delete()
-                            embinfo = discord.Embed(description=f"<:invite:1105878276242673725> Заявка игрока {memberop.mention} \n\n**Статус заявки:** <:minecraft_deny:1080779495386140684> Отклонена. \n**Отклонил заявку:** {m.author.mention}.", color = 0x2f3136)
+                            embinfo = discord.Embed(description=f"<:invite:1105878276242673725> Заявка пользователя {memberop.mention} ({memberop.display_name}) \n\n**Статус заявки:** <:minecraft_deny:1080779495386140684> Отклонена \n**Отклонил заявку:** {m.author.mention}", color = 0x2f3136)
                             for key, value in inter.text_values.items():
                                 embinfo.add_field(name=key.capitalize(), value=value[:1024], inline=False)  
                             await logchannel.send(embed=embinfo)
